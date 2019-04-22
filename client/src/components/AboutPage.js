5fb4a514d843c5ed3394c4a74b3e11f3ff77e54f
Ingrid G <aleja.morada@gmail.com>
2019-04-22 08:52:30 -0700

    Added Bootstrap and started component folder

---
+++ client/src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class AboutPage extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>About Page!</h1>
+                <button type="button" class="btn btn-primary">Login</button>
+                <button type="button" class="btn btn-secondary">Sign up!</button>
+            </div>
+        )
+    }
+}
+
+export default AboutPage;
