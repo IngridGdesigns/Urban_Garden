795b95b360dee24e19376bf5e01f33712d7389b6
Ingrid G <aleja.morada@gmail.com>
2019-05-05 12:20:18 -0700

    Finally got momentjs time format

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-// import { Panel, Label} from 'react-bootstrap';
 import { FaLemon } from 'react-icons/fa';
 import Moment from 'react-moment';
 import './Profile.css';
-// import AddPosts from '../AddPosts';
-// import Dashboard from '../Dashboard';
+import axios from 'axios'
+
 
 class Profile extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      profile: {}
-    }
-  }
+ 
+
   componentWillMount() {
     this.setState({ profile: {} });
     const { userProfile, getProfile } = this.props.auth;
@@ -21,6 +16,7 @@ class Profile extends Component {
         this.setState({ profile });
       });
     } else {
+      console.log({userProfile})
       this.setState({ profile: userProfile });
     }
   }
@@ -36,29 +32,35 @@ class Profile extends Component {
 //     // body: JSON.stringify(data);
 //   })
 // }
-postingToDB = ({userProfile}) => {
-  console.log(`is it working now?? ${userProfile}`)
-  const headers = { 'Authorization': `Bearer ${this.props.auth.accessToken}`, 'Content-Type': 'application/json'}
-  const data = { userProfile }
-  console.log(data + 'is this even working?')
-  fetch('http://localhost:3005/usersdata', {
-          method: 'POST',
-          headers: headers,
-          body: JSON.stringify(data),
+
+
+postingToDB = () => { //CHISOM HELPED
+  const { getProfile, getAccessToken } = this.props.auth;
+  const headers = { 'Authorization': `Bearer ${getAccessToken}`}
+ 
+  getProfile((err, profile) => {
+    console.log(profile + "its not posting")
+    if(err){
+      console.log(err)
+    } else {
+      axios({
+        method: 'POST',
+        headers,
+        url: 'http://localhost:3005/usersdata',
+        data: profile
       })
-      .then(res => res.json(), (console.log(data + 'not working')))
-  }
+    }
+  });
+}
 
-
-  // getProfile(cb) {
-  //   this.auth0.client.userInfo(this.accessToken, (err, profile) => {
-  //     if (profile) {
-  //       this.userProfile = profile;
-  //     }
-  //     cb(err, profile);
-  //   });
+  // console.log(data + 'is this even working?')
+  // fetch('http://localhost:3005/usersdata', {
+  //         method: 'POST',
+  //         headers: headers,
+  //         body: JSON.stringify(data),
+  //     })
+  //     .then(res => res.json(), (console.log(data + 'not working')))
   // }
-
 
 
 
@@ -67,31 +69,39 @@ postingToDB = ({userProfile}) => {
     return (
       <div className="container">
         <div className="profile-area">
-          {/* <h1>{profile.name}</h1> */}
+          <h1>{profile.name}</h1>
           <header header="Profile">
             <img src={profile.picture} alt="profile" />
             <div>
               <label><FaLemon glyph="user" /> Donut Evangelist</label>
               <h2>{profile.nickname}</h2>
               <span>
-                    <Moment //Displays from a library called moment to change display of date/time
+                <Moment
+              
+                  
+                     //Displays from a library called moment to change display of date/time 
                         date={profile.updated_at}
-                        parse='YYYY-MM-dd hh:mm' //what it looks like in json
-                        format='MMM-D h:mma'
-                    //how you want it to look like, Month-, day, hour minute, an a for am
+                        parse={profile.updated_at}
+                        // parse='YYYY-MM-dd hh:mm' //what it looks like in json
+                        format='MMM-D'
+                        />
+                    {/* //how you want it to look like, Month-, day, hour minute, an a for am
                     //will look like: Nov-1 10:15am instead of 2019-11-10:15 12
-                    />
+                    /> */}
                 </span>
             </div>
             {/* json string stuff */}
+            <button onClick={this.postingToDB}>posting user button</button>
             <pre>{JSON.stringify(profile, null, 2)}</pre> 
           </header>
+         
         </div>
        {/* <AddPosts  auth={this.props.auth}/> */}
       </div>
     );
   }
 }
+
 
 export default Profile;
 // console.log(userProfile + '123456')    
