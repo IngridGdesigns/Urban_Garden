4b7094192fe76e1db93ab25f1ee17d1240b13545
Ingrid G <aleja.morada@gmail.com>
2019-05-17 01:07:00 -0700

    Changed home page for user and style

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import {Link} from 'react-router-dom'
-import {FaRecycle, FaCarrot,FaUsers } from 'react-icons/fa';
+import {FaRecycle, FaCarrot, FaUsers, FaHeart} from 'react-icons/fa';
 // import Alert from 'react-bootstrap/Alert'
 
 
@@ -11,46 +11,46 @@ class Home extends React.Component {
     render(){
         return (
             <div>
-              
              <div className='container'style={{paddingBottom: '10px'}}>
-              <div className='row'>
-              <div>
-                <h1 className='color1'>Welcome to Urban Garden!</h1>
-                <h4>Let's get started! You are more than welcome to visit our 
-                </h4>
-                </div>
-              </div>
-            </div> 
-          
-              
-              <div className='container' style={{paddingTop: '65px', marginTop: '50px'}}>
-                
-                <div className='centered'>
-                {/* <h2 className='color3'>Our mission: </h2> */}
-                <section className='row content' style={{paddingTop:'25px'}}>
-                
-                  <div className='col-sm-6 col-md-4 color3' >
-                  
-                    <h4><FaUsers style={{color: 'grey'}}/> Build Community</h4>
-                    
-                    <p>Make friends and barter for some yummy avocados!</p>
-                  </div>
-                  <div className='col-sm-6 col-md-4 color3' >
+ 
+            <section className='view'>
 
-                    <h4><FaRecycle style={{color: 'skyblue'}}/> Barter for a change!</h4>
+            <div className='row'>
+
+              <div className='col-md-6'>
+
+                <div className='d-flex flex-column justify-content-center align-items-center h-100'>
+                  <h1 className='heading display-3 color1'>Welcome to Urban Garden!</h1>
+                  <h4 className='subheading font-weight-bold'>
+                    You are more than welcome to visit our barter page
+                    to start seeing great opportunities to swap fresh food
+                    with awesome people.
                     
-                    <p>If you have an abundance of rosemary, why not barter for something
-                      you may want?
-                    </p>
+                  </h4>
+                  <div className='mr-auto'>
+                    <button type='button' 
+                    className='btn btn-lily btn-margin btn-rounded'>Get started 
+                    <FaHeart style={{color: 'pink', paddingLeft: '3px'}}/></button>
                   </div>
-                  <div className='col-sm-6 col-md-4 color3 '>
-                    <h4><FaCarrot style={{color: 'orange'}} /> Reduce food waste</h4>
-                    <p>Keep giving to other who want food you don't want anymore.</p>
-                  </div>
-                  </section>
                 </div>
+
+              </div>
+
+              <div className='col-md-6'>
+
+                <div className='view'>
+                  <img className='img-fluid' 
+                    src='https://images.pexels.com/photos/1586529/pexels-photo-1586529.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'  alt='lemonade'/>
+                  {/* <div className='mask flex-center hm-gradient'>
+                  <caption>yummy lemonade</caption>
+                  </div> */}
                 </div>
+
+              </div>
             </div>
+          </section>
+        </div>
+    </div>
             
         )
     }
